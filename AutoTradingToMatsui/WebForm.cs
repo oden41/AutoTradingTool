7b39569805240ff46418aea2c29d9dcd6f80f56d
oden <shumpmita@ic.dis.titech.ac.jp>
2017-07-30 20:46:06 +0900

    * 資産状況を見るメソッド * 前営業日を取得するメソッド * サイン点灯銘柄を確認するメソッド を追加

--- AutoTradingToMatsui/WebForm.cs
+++ AutoTradingToMatsui/WebForm.cs
@@ -4,11 +4,13 @@ using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Utility;
 
 namespace AutoTradingToMatsui
 {
@@ -19,13 +21,31 @@ namespace AutoTradingToMatsui
         private string ID { get { return ConfigurationManager.AppSettings["ID"]; } }//ログインID
         private string PASSWORD { get { return ConfigurationManager.AppSettings["PASSWORD"]; } }//ログインパスワード
         private string TRADEPASS { get { return ConfigurationManager.AppSettings["TRADEPASSWORD"]; } }//取引パス
+        private double ONEDAYTRADERATIO = 0.3;//1日に投資する最大金額(全資産比)
 
         //銘柄コード -> [銘柄名,保有株数,取得平均,評価単価]
         private Dictionary<string, object[]> portfolio;
+        //データベース
+        private DataBase db;
+        //<戦略sql,保有日数>のペア
+        private List<KeyValuePair<string, int>> strategyList;
+        //売買予定リスト
+        private List<object[]> tradeList;
+        private double CashAmount = -1;
+        private double AllAssetAmount = -1;
 
         public WebForm()
         {
             InitializeComponent();
+            db = new DataBase();
+
+            strategyList = new List<KeyValuePair<string, int>>();
+            //GPR4.8,上昇局面タイプだがリーマンショックから早く回復
+            strategyList.Add(new KeyValuePair<string, int>("../../SQL/20170530.sql", 10));
+            //GPR4近い，上昇局面に強い
+            strategyList.Add(new KeyValuePair<string, int>("../../SQL/20170612.sql", 20));
+
+            tradeList = new List<object[]>();
         }
 
         private void WebForm_Load(object sender, EventArgs e)
@@ -70,7 +90,7 @@ namespace AutoTradingToMatsui
         {
             try
             {
-                if (webBrowser.Url.AbsoluteUri != mainURL)
+                if (webBrowser.Url?.AbsoluteUri != mainURL)
                     Login();
                 //株式取引選択
                 var topCollection = webBrowser.Document.GetElementsByTagName("a");
@@ -150,7 +170,7 @@ namespace AutoTradingToMatsui
         {
             try
             {
-                if (webBrowser.Url.AbsoluteUri != mainURL)
+                if (webBrowser.Url?.AbsoluteUri != mainURL)
                     Login();
                 //株式取引選択
                 var topCollection = webBrowser.Document.GetElementsByTagName("a");
@@ -221,7 +241,7 @@ namespace AutoTradingToMatsui
         {
             try
             {
-                if (webBrowser.Url.AbsoluteUri != mainURL)
+                if (webBrowser.Url?.AbsoluteUri != mainURL)
                     Login();
 
                 if (portfolio == null)
@@ -258,6 +278,93 @@ namespace AutoTradingToMatsui
                 return false;
             }
             return true;
+        }
+
+
+        /// <summary>
+        /// DBを見に行き，前営業日にサインが点灯しているかをチェックする
+        /// 存在していたらtradeListに銘柄コードと終値を追加
+        /// </summary>
+        public void CheckSign()
+        {
+            var asOf = GetPrevWorkDay(DateTime.Today);
+            //銘柄コード，終値，保有日数
+            var list = new List<object[]>();
+            foreach (var strategy in strategyList)
+            {
+                var sql = "";
+                using (StreamReader sr = new StreamReader(strategy.Key, Encoding.UTF8))
+                {
+                    sql = sr.ReadToEnd();
+                }
+                var result = db.ExecuteReader(sql)?.Rows.Cast<DataRow>();
+                if (result == null)
+                    continue;
+                var filtered = result.Where(row => DateTime.Parse(row["AsOfDate"].ToString()) == asOf).ToList();
+
+                filtered.ForEach(row => tradeList.Add(new object[] { row["CodeNum"].ToString(), double.Parse(row["NowPrice"].ToString()) }));
+            }
+        }
+
+
+        /// <summary>
+        /// 資産状況をチェックするメソッド
+        /// </summary>
+        public bool CheckAssetAmount()
+        {
+            try
+            {
+                if (webBrowser.Url?.AbsoluteUri != mainURL)
+                    Login();
+
+                //資産状況選択
+                var topCollection = webBrowser.Document.GetElementsByTagName("a");
+                topCollection.Cast<HtmlElement>().Where(e => e.InnerText == "資産状況").First().InvokeMember("click");
+                Wait();
+
+                //資産状況一覧選択
+                var sellCollection = webBrowser.Document.GetElementsByTagName("a");
+                sellCollection.Cast<HtmlElement>().Where(e => e.InnerText == "資産状況一覧").First().InvokeMember("click");
+                Wait();
+
+                var text = webBrowser.Document.Body.InnerText;
+                var split = text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                CashAmount = double.Parse(split[6].Replace("円","").Replace(",",""));
+                AllAssetAmount = double.Parse(split[3].Replace("円", "").Replace(",", ""));
+            }
+            catch (Exception e)
+            {
+                CashAmount = -1;
+                AllAssetAmount = -1;
+                return false;
+            }
+            return true;
+        }
+
+
+        /// <summary>
+        /// 本来は休日リストをDBに作るべきだが，今回はデータを追加した最終日を取得
+        /// </summary>
+        /// <param name="today"></param>
+        /// <returns></returns>
+        public DateTime GetPrevWorkDay(DateTime today)
+        {
+            var sql = $"SELECT LastDate FROM GPrediction.dbo.Utils WHERE id = 0";
+            var date = db.ExecuteScalar(sql, new DateTime(1900, 1, 1));
+            if (date.Year != 1900)
+                return date;
+            else
+            {
+                var result = today;
+                while (true)
+                {
+                    result = result.AddDays(-1);
+                    if (result.DayOfWeek != DayOfWeek.Saturday && result.DayOfWeek != DayOfWeek.Sunday)
+                        break;
+                }
+                return result;
+            }
         }
 
 
