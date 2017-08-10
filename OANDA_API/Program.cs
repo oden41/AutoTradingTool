fc1f325a5f1d7b4012c58bba75adf49a2f4cca03
oden <shumpmita@ic.dis.titech.ac.jp>
2017-08-10 11:48:26 +0900

    * FXのOANDA，APIを試すファイル追加

---
+++ OANDA_API/Program.cs
@@ -0,0 +1,79 @@
+﻿using RestApi;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Utility;
+
+namespace OANDA_API
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var db = new DataBase();
+            var id = int.Parse(ConfigurationManager.AppSettings["AccountId"]);
+            //var result = Rest.GetAccountDetails(8048648);
+            //var result = Rest.GetInstruments(id);
+            //var result = Rest.GetPositions(id);
+            //var result = Rest.GetTradeList(id);
+            //var result = Rest.GetCandles("USD_JPY", id,count:5000);
+            //var result = Rest.GetOrderList(id);
+            //var dir = new Dictionary<string, string>();
+            //dir.Add("instrument", "USD_JPY");
+            //dir.Add("units", "10000");
+            //dir.Add("side", "sell");
+            //dir.Add("type", "market");
+            //Rest.PostMarketOrder(id, dir);
+
+            var start = new DateTime(2005, 1, 1);
+            var last = new DateTime(2017, 1, 1);
+            while (start < last)
+            {
+                var result = Rest.GetCandles("USD_JPY", id, start, 5000, granularity: "M10");
+                result.ForEach(data =>
+                {
+                    var insertSQL = $@"
+INSERT INTO USDJPY_10m(
+time,
+openBid,
+openAsk,
+highBid,
+highAsk,
+lowBid,
+lowAsk,
+closeBid,
+closeAsk,
+openMid,
+highMid,
+lowMid,
+closeMid
+)
+VALUES
+(
+'{data.time.ToString("yyyy-MM-dd hh:mm:ss")}',
+{data.openBid},
+{data.openAsk},
+{data.highBid},
+{data.highAsk},
+{data.lowBid},
+{data.lowAsk},
+{data.closeBid},
+{data.closeAsk},
+{data.openMid},
+{data.highMid},
+{data.lowMid},
+{data.closeMid}
+)
+";
+                    db.ExecuteNonQuery(insertSQL);
+                });
+                start = result.Last().time;
+            }
+        }
+    }
+}
---
+++ Utility/DataBase.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public class DataBase
+    {
+        public string ConnString
+        {
+            get { return ConfigurationManager.AppSettings["ConnString"]; }
+        }
+
+
+        /// <summary>
+        /// SQLにより発行したテーブルの1行1列目を返すメソッド
+        /// </summary>
+        /// <param name="SQL">SQL文</param>
+        /// <param name="def">エラー時に返す値</param>
+        /// <returns></returns>
+        public T ExecuteScalar<T>(string SQL, T def)
+        {
+            using (var conn = new SqlConnection(ConnString))
+            {
+                return ExecuteScalar(conn, SQL, def);
+            }
+        }
+
+
+        public T ExecuteScalar<T>(SqlConnection conn, string SQL, T def)
+        {
+            // SQLを実行します。
+            try
+            {
+                if (conn.State != ConnectionState.Open)
+                    conn.Open();
+
+                var command = new SqlCommand(SQL, conn);
+                command.Connection = conn;
+                return (T)Convert.ChangeType(command.ExecuteScalar(), typeof(T));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.WriteLine($"SQL:{SQL}");
+                return def;
+            }
+        }
+
+        /// <summary>
+        /// SQL(SELECT)により得られたDataTableを返すメソッド
+        /// </summary>
+        /// <param name="SQL"></param>
+        /// <returns></returns>
+        public DataTable ExecuteReader(string SQL)
+        {
+            var dt = new DataTable();
+
+            using (var conn = new SqlConnection(ConnString))
+            {
+                return ExecuteReader(conn, SQL);
+            }
+        }
+
+
+        public DataTable ExecuteReader(SqlConnection conn, string SQL)
+        {
+            try
+            {
+                var dt = new DataTable();
+                if (conn.State != ConnectionState.Open)
+                    conn.Open();
+
+                var command = new SqlCommand(SQL, conn);
+                command.Connection = conn;
+                command.CommandTimeout = 30;
+                // SQLを実行します。
+                var reader = command.ExecuteReader();
+                // 型付テーブルを作成します。
+                dt = CreateSchemaDataTable(reader);
+                while (reader.Read())
+                {
+                    var row = dt.NewRow();
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        row[i] = reader.GetValue(i);
+                    }
+                    dt.Rows.Add(row);
+                }
+                return dt;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.WriteLine($"SQL:{SQL}");
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// INSERTやUPDATEなど，DBに書き込むためのメソッド
+        /// </summary>
+        /// <param name="SQL"></param>
+        /// <returns>成功:true,失敗:false</returns>
+        public bool ExecuteNonQuery(string SQL)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnString))
+            {
+                conn.Open();
+
+                SqlCommand command = new SqlCommand(SQL, conn);
+                command.Connection = conn;
+                // SQLを実行します。
+                try
+                {
+                    command.ExecuteNonQuery();
+                    return true;
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                    Console.WriteLine($"SQL:{SQL}");
+                    return false;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// SqlDataReaderで取得した構造を元にDataTableを作成します。
+        /// </summary>
+        /// <param name="reader">SqlDataReaderオブジェクト</param>
+        /// <returns>DataTableオブジェクト</returns>
+        /// <remarks>
+        /// 参考：
+        /// http://msdn.microsoft.com/ja-jp/library/system.data.datatablereader.getschematable.aspx
+        /// </remarks>
+        private DataTable CreateSchemaDataTable(SqlDataReader reader)
+        {
+            if (reader == null) { return null; }
+            if (reader.IsClosed) { return null; }
+
+            DataTable schema = reader.GetSchemaTable();
+            DataTable dt = new DataTable();
+
+            foreach (DataRow row in schema.Rows)
+            {
+                // Column情報を追加してください。
+                DataColumn col = new DataColumn();
+                col.ColumnName = row["ColumnName"].ToString();
+                col.DataType = Type.GetType(row["DataType"].ToString());
+
+                if (col.DataType.Equals(typeof(string)))
+                {
+                    col.MaxLength = (int)row["ColumnSize"];
+                }
+
+                dt.Columns.Add(col);
+            }
+            return dt;
+        }
+    }
+}
